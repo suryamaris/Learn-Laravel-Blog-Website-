b762bc15c76b52445f4eed53b77bc2f4a3630538
suryamaris <smarispanensa@gmail.com>
2022-03-15 15:38:08 +0700

    Searching dan Pagination

--- routes/web.php
+++ routes/web.php
@@ -43,18 +43,3 @@ Route::get('categories', function(){
         'categories' => Category::all()
     ]);
 });
-
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts',[
-        'title' => "Post by Category : $category->name",
-        "active" => "categories",
-        'posts' => $category->posts 
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[
-        'title' => "Post by Author : $author->name", 
-        'posts' => $author->posts->load('category','user')
-    ]);
-}); 
