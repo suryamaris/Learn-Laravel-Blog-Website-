f2907047dc1f9c7987883908c7241eac83f39c76
suryamaris <smarispanensa@gmail.com>
2022-04-13 12:55:19 +0700

    Update dan delete gambar

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller
@@ -102,6 +103,7 @@ class DashboardPostController extends Controller
         $rules =[
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
 
@@ -109,7 +111,15 @@ class DashboardPostController extends Controller
             $rules['slug'] = 'required|unique:posts';
         }
 
+        
         $validateData = $request->validate($rules); 
+        
+        if($request->file('image')){
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+            $validateData['image'] = $request->file('image')->store('post-images');
+        }
         $validateData['user_id'] = auth()->user()->id;
         $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
@@ -127,6 +137,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $Post)
     {
+        if($Post->image){
+            Storage::delete($Post->image);
+        }
         Post::destroy($Post->id);        
         return redirect('/dashboard/posts')->with('success', 'New post has been deleted!');
     }
